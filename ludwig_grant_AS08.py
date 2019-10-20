08bed66142ea80cd47ac28e5c6d7c659f365e7a0
unknown <grantludwig97@gmail.com>
2019-10-19 21:35:05 -0700

    initial commit

---
+++ ludwig_grant_AS08.py
@@ -0,0 +1,20 @@
+# File Name: ludwig_grant_AS08.py
+# File Path: /home/ludwigg/Python/PyRpi_AS8/ludwig_grant_AS08.py
+# Run Command: sudo python3 /home/ludwigg/Python/PyRpi_AS8/ludwig_grant_AS08.py
+
+# Grant Ludwig
+# 10/23/2019
+# AS.08
+# HCreate a target shooting game
+
+from graphics import *
+
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+
+win = GraphWin("Ball Bounce", SCREEN_WIDTH, SCREEN_HEIGHT, autoflush=False)
+
+def main():
+    win.setBackground("Grey")
+
+main()
